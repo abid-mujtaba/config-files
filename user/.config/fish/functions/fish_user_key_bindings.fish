8831ac9bfb81effe9979d0c4376b5121d44a7f40
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2019-04-19 21:42:54 -0400

    Add fzf bindings to fish

--- user/.config/fish/functions/fish_user_key_bindings.fish
+++ user/.config/fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,8 @@ function fish_user_key_bindings
     for mode in insert default visual
     	bind -M $mode \cf forward-char
 	end
+
+	fzf_key_bindings
 end
 
 # Source: https://stackoverflow.com/a/48958650/2926226
